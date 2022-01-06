0e143fdca36dc245e390a9acc0a4ba442dbcf915
unknown <mfaqihuddin23@gmail.com>
2022-01-06 11:33:53 +0700

    text editor added still trouble

---
+++ assets/js/cm-js.js
@@ -0,0 +1,27 @@
+
+// CodeMirror(document.querySelector('#my-div'), {
+//     lineNumbers: true,
+//     tabSize: 2,
+//     value: '[{"id":1,"name":"David","age":20},{"id":2,"name":"Bobby","age":34},{"id":3,"name":"Warren","age":28}]',
+//     mode: { name: "javascript", json: true },
+//     theme: 'monokai',
+//     // scrollbarStyle: "simple",
+//     readOnly: true
+//   });
+var editor = CodeMirror.fromTextArea(document.getElementById("mytextarea"), {
+  lineNumbers: false,
+  matchBrackets: false,
+  mode: { name: "javascript", json: true },
+  // value: TextArea.value,
+  theme: 'monokai',
+  readOnly: true
+});
+
+var editor = CodeMirror.fromTextArea(document.getElementById("mytextarea2"), {
+  lineNumbers: false,
+  matchBrackets: false,
+  mode: { name: "javascript", json: true },
+  // value: TextArea.value,
+  theme: 'monokai',
+  readOnly: true
+});

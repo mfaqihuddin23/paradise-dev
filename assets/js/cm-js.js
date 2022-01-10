38909c3dcd3e82b6aeedd86cd3bb9e4539c9f4ea
unknown <mfaqihuddin23@gmail.com>
2022-01-10 11:58:42 +0700

    mobile tab preview

--- assets/js/cm-js.js
+++ assets/js/cm-js.js
@@ -12,7 +12,7 @@ var editor = CodeMirror.fromTextArea(document.getElementById("mytextarea"), {
   lineNumbers: false,
   matchBrackets: false,
   mode: { name: "javascript", json: true },
-  // value: TextArea.value,
+  // value: '[{"id":1,"name":"David","age":20},{"id":2,"name":"Bobby","age":34},{"id":3,"name":"Warren","age":28}]',
   theme: 'monokai',
   readOnly: true
 });

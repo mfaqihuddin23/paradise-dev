5476a6b7331127adf3af969fbf3d0e8ce8e492ee
unknown <mfaqihuddin23@gmail.com>
2022-01-12 15:02:13 +0700

    codemirror fix

--- assets/js/cm-js.js
+++ assets/js/cm-js.js
@@ -10,18 +10,80 @@
 //   });
 var editor = CodeMirror.fromTextArea(document.getElementById("mytextarea"), {
   lineNumbers: false,
-  matchBrackets: false,
   mode: { name: "javascript", json: true },
-  // value: '[{"id":1,"name":"David","age":20},{"id":2,"name":"Bobby","age":34},{"id":3,"name":"Warren","age":28}]',
   theme: 'monokai',
+  scrollbarStyle: "simple",
+  readOnly: true
+});
+var editor = CodeMirror.fromTextArea(document.getElementById("mytextarea1"), {
+  lineNumbers: false,
+  mode: { name: "javascript", json: true },
+  theme: 'monokai',
+  scrollbarStyle: "simple",
+  readOnly: true
+});
+var editor = CodeMirror.fromTextArea(document.getElementById("mytextarea2"), {
+  lineNumbers: false,
+  mode: { name: "javascript", json: true },
+  theme: 'monokai',
+  scrollbarStyle: "simple",
+  readOnly: true
+});
+var editor = CodeMirror.fromTextArea(document.getElementById("mytextarea3"), {
+  lineNumbers: false,
+  mode: { name: "javascript", json: true },
+  theme: 'monokai',
+  scrollbarStyle: "simple",
+  readOnly: true
+});
+var editor = CodeMirror.fromTextArea(document.getElementById("mytextarea4"), {
+  lineNumbers: false,
+  mode: { name: "javascript", json: true },
+  theme: 'monokai',
+  scrollbarStyle: "simple",
+  readOnly: true
+});
+var editor = CodeMirror.fromTextArea(document.getElementById("mytextarea5"), {
+  lineNumbers: false,
+  mode: { name: "javascript", json: true },
+  theme: 'monokai',
+  scrollbarStyle: "simple",
+  readOnly: true
+});
+var editor = CodeMirror.fromTextArea(document.getElementById("mytextarea6"), {
+  lineNumbers: false,
+  mode: { name: "javascript", json: true },
+  theme: 'monokai',
+  scrollbarStyle: "simple",
+  readOnly: true
+});
+var editor = CodeMirror.fromTextArea(document.getElementById("mytextarea7"), {
+  lineNumbers: false,
+  mode: { name: "javascript", json: true },
+  theme: 'monokai',
+  scrollbarStyle: "simple",
+  readOnly: true
+});
+var editor = CodeMirror.fromTextArea(document.getElementById("mytextarea8"), {
+  lineNumbers: false,
+  mode: { name: "javascript", json: true },
+  theme: 'monokai',
+  scrollbarStyle: "simple",
+  readOnly: true
+});
+var editor = CodeMirror.fromTextArea(document.getElementById("mytextarea9"), {
+  lineNumbers: false,
+  mode: { name: "javascript", json: true },
+  theme: 'monokai',
+  scrollbarStyle: "simple",
   readOnly: true
 });
 
-var editor = CodeMirror.fromTextArea(document.getElementById("mytextarea2"), {
+
+var editor = CodeMirror.fromTextArea(document.getElementById("mytextarea-b"), {
   lineNumbers: false,
   matchBrackets: false,
   mode: { name: "javascript", json: true },
-  // value: TextArea.value,
   theme: 'monokai',
   readOnly: true
 });
